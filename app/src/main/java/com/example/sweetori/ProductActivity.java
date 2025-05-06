5d5c444effe376b3734abf6fd100b41a67c79783
ThuHuynh1438 <thu0938905081@gmail.com>
2025-05-07 04:17:18 +0700

    voucher detail ui, activity prod, prodtl, voucherdtl

---
+++ app/src/main/java/com/example/sweetori/ProductActivity.java
@@ -0,0 +1,61 @@
+package com.example.sweetori;
+
+import android.os.Bundle;
+
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.HorizontalScrollView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ProductActivity extends AppCompatActivity {
+
+    ImageView btnAccount;
+    ImageView btnHome;
+    ImageView btnCart;
+    ImageView btnNoti;
+    ImageView btnVoucher;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.product);
+        EdgeToEdge.enable(this);
+
+        //Component
+        btnAccount = findViewById(R.id.btnAccount);
+        btnHome = findViewById(R.id.btnHome);
+        btnCart = findViewById(R.id.btnCart);
+        btnNoti = findViewById(R.id.btnNoti);
+        btnVoucher = findViewById(R.id.btnVoucher1);
+
+        //Intent
+        btnAccount.setOnClickListener(v -> {
+            Intent account = new Intent(ProductActivity.this, AccountActivity.class);
+            startActivity(account);
+        });
+        btnHome.setOnClickListener(v -> {
+            Intent home = new Intent(ProductActivity.this, MainActivity.class);
+            startActivity(home);
+        });
+        btnCart.setOnClickListener(v -> {
+            Intent cart = new Intent(ProductActivity.this, CartActivity.class);
+            startActivity(cart);
+        });
+        btnNoti.setOnClickListener(v -> {
+            Intent noti = new Intent(ProductActivity.this, NotiActivity.class);
+            startActivity(noti);
+        });
+        btnVoucher.setOnClickListener(v -> {
+            Intent voucher = new Intent(ProductActivity.this, VoucherActivity.class);
+            startActivity(voucher);
+        });
+    }
+}
